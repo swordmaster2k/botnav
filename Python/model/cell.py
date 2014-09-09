07016ae62548b4699477a0cec23bd72ad11972cf
swordmaster2k <b00056835@student.itb.ie>
2014-09-09 22:07:44 +0100

    + Added cell module.

---
+++ Python/model/cell.py
@@ -0,0 +1,16 @@
+'''
+
+'''
+class Cell:
+
+    '''
+
+    '''
+    def __init__(self, x, y, data):
+        self.x = x
+        self.y = y
+        self.data = data
+
+    def to_string(self):
+        print("x: " + str(self.x)  + ", y: " + str(self.y) + ", data: " + str(self.data))
+    
